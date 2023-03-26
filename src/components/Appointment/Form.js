5abca0cbd6549f50f7cfd615a8be12eba454a7d6
Andre <andrepaesbm@gmail.com>
2023-03-25 21:01:06 -0600

    add Form component and use state to hook changes into the main form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function (props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  const reset = function () {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = function () {
+    reset();
+    props.onCancel();
+  };
+
+  const validate = function () {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
+    setError("");
+    props.onSave(student, interviewer);
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            data-testid="student-name-input"
+          />
+          <section className="appointment_validation">{error}</section>
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
