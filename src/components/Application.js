1d60d765ee54134a39c62a28b95d72e79da2dc5d
Andre <andrepaesbm@gmail.com>
2023-03-28 15:27:50 -0600

    remove hard-coded days variable and now using an useEffect to retrieve the setDays Data instead

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import "components/DayListItem.scss";
 import Appointment from "components/Appointment";
@@ -43,26 +44,16 @@ const appointments = {
   }
 };
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const [days, setDays] = useState([]);
+  
+  useEffect(() => {
+    axios.get("/api/days").then(response => {
+      setDays(response.data);
+    });
+  }, []);
 
   return (
     <main className="layout">
