6810cad64cb81219bf54ea097858bbba583c5c03
Andre <andrepaesbm@gmail.com>
2023-03-30 22:23:29 -0600

    add getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,11 @@ export function getAppointmentsForDay(state, day) {
   );
   return appointments;
 }
+
+export function getInterview(state,interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewObj = {"student": interview.student, "interviewer": state.interviewers[interview.interviewer]};
+  return interviewObj;
+};
