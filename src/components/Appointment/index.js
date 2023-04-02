68e45c7f75ddcb7455a6ae27d64e3fcd9679d471
Andre <andrepaesbm@gmail.com>
2023-04-02 10:50:51 -0600

    installed testing-library react-hooks, create the tests and update the appointment component to use the custom hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,21 +3,39 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CONFIRM = "CONFIRM";
+const CREATE = "CREATE";
+const EDIT = "EDIT";
+const SAVE = "SAVE";
+
 
 export default function Appointment(props) {
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
-      <Header time={props.time} />
-      {props.isLast && <div className="appointment__add-button">+</div>}
-      {props.interview ? (
-        <Show
-          student={props.interview.student}
+     <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && props.interview && (<Show
           interviewer={props.interview.interviewer}
-          onEdit={props.onEdit}
-          onDelete={props.onDelete}
+          student={props.interview.student}
+          onEdit={() => transition(EDIT) }
+          onDelete={() => transition(CONFIRM)}
+        /> 
+      )}
+      {mode === CREATE && (<Form 
+          interviewers={[]}
+          onCancel={() => back()}
+          onSave={ () => transition(SAVE)}
         />
-      ) : (
-        <Empty onAdd={props.onAdd} />
       )}
     </article>
   );
