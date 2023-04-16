7bf69ce7d36b8790b269b72dd4c21cbfc75708e0
Andre <andrepaesbm@gmail.com>
2023-04-15 20:40:37 -0600

    created tests for the appointment component

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Appointment from "/components/index"
+
+
+describe("Appointment", () => {
+  xit("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  xit("does something it is supposed to do", () => {
+    // ...
+  });
+
+  xit("does something else it is supposed to do", () => {
+    // ...
+  });
+});
